beec818a648dcf177ea8d5affa1c8c97a2072632
Guiselly Aroche <guisellyaroche@outlook.com>
2021-02-26 04:16:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,3 +11,11 @@ def valid_move?(board, index)
       return true
    end
 end
+
+def position_taken?(board, index)
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false 
+   else
+      return true
+   end
+end
